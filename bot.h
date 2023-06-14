ef449234bd6c6e60755831787e29520cd0db91ad
manlinh <135205672+manlinh@users.noreply.github.com>
2023-06-14 20:41:24 +0800

    Update bot.h

--- bot.h
+++ bot.h
@@ -2,6 +2,12 @@
 #define _BOT_H
 #include "structure.h"
 #include "vectorInt.h"
-int botOption(int state, player *Players, int index, piece *lands, int haveK);
+int botOption(int state, player *Players, int index, piece *lands);
 int botRobber(piece *land, int playerID);
+void botChooseBestRoad(piece *p, player *players, int index, int *landID,
+                       int *roadID);
+void botChooseBestSwttlement(piece *p, player *players, int index, int *landID,
+                             int *roadID);
+void botChooseBestCity(piece *p, player *players, int index, int *landID,
+                       int *roadID);
 #endif
